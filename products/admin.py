57bd6f21438c41dffdc36276074a7cb05e8e8081
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-13 12:23:18 +0000

    add admin custom products app views

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,39 @@
 from django.contrib import admin
 from .models import Division, Product, Category
 
-# Register your models here.
-admin.site.register(Division)
-admin.site.register(Product)
-admin.site.register(Category)
+
+class DivisionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'division',
+        'name',
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'sku',
+        'brand',
+        'name',
+        'size',
+        'amount',
+        'color',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Division, DivisionAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
