de1af0559110d7f3916e3968f07447d64f019776
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-13 11:56:05 +0000

    made migrations and registered to models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Division, Product, Category
 
 # Register your models here.
+admin.site.register(Division)
+admin.site.register(Product)
+admin.site.register(Category)
