f9c6453f40e19cde1217ec101262c3f09486052b
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-18 17:35:37 +0000

    Add initial profile page

--- profiles/models.py
+++ profiles/models.py
@@ -12,7 +12,9 @@ class UserProfile(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_full_name = models.CharField(max_length=50, null=False, blank=False)    
+    default_full_name = models.CharField(max_length=50, null=True, blank=True)
+    default_email = models.EmailField(max_length=254, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)   
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
