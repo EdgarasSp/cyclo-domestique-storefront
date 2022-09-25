9629876a885aa70d26475aa26fd7c655e323c2f7
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-25 16:07:50 +0000

    Add 404 page

--- cyclo_domestique/urls.py
+++ cyclo_domestique/urls.py
@@ -29,3 +29,5 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'boutique_ado.views.handler404'
