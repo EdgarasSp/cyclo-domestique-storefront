e2c99164b97786e3d9d197ba3985d082557a75f2
Edgaras Spiridonov <dev.applications.esp@gmail.com>
2022-09-25 12:02:21 +0000

    Update settings.py secret key and debug from environment

--- cyclo_domestique/settings.py
+++ cyclo_domestique/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1a4fr#2fzep9t2vwfkwm(tz==9bk)9@q$yllgc(y@#sfx(76bd'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cyclo-domestique.herokuapp.com', 'localhost']
 
@@ -135,17 +135,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-
-DATABASES = {
-    'default':{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
-
 
 
 
